0e014b458d3559c21cc290aa56ee38eb4d23f141
unknown <you@example.com>
2025-09-06 17:58:56 -0700

    WEEK 6 SUBMISSIONS

---
+++ answers.sql
@@ -0,0 +1,36 @@
+#question 1:
+SELECT 
+    employees.firstName, 
+    employees.lastName, 
+    employees.email, 
+    employees.officeCode
+FROM 
+    employees
+INNER JOIN 
+    offices ON employees.officeCode = offices.officeCode;
+
+
+#question 2:
+SELECT 
+    products.productName, 
+    products.productVendor, 
+    products.productLine
+FROM 
+    products
+LEFT JOIN 
+    productlines ON products.productLine = productlines.productLine;
+
+
+question 3:
+SELECT 
+    orders.orderDate, 
+    orders.shippedDate, 
+    orders.status, 
+    orders.customerNumber
+FROM 
+    customers
+RIGHT JOIN 
+    orders ON customers.customerNumber = orders.customerNumber
+ORDER BY 
+    orders.orderDate
+LIMIT 10;
